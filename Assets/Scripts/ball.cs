9c7e9a4cda22cb96169096336347edb8dfbe9ca3
jukov_yuti <--->
2020-10-27 20:55:15 +0300

    Initial commit

---
+++ Assets/Scripts/ball.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ball : MonoBehaviour
+{
+
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+		print("enter!");
+	}
+
+	private void OnCollisionExit2D(Collision2D collision)
+	{
+		print("exit!");
+	}
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		print("trigger enter!");
+	}
+
+	private void OnTriggerExit2D(Collider2D collision)
+	{
+		print("trigger exit!");
+	}
+
+}
